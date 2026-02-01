a761d8b7da43bc3196bb97abf668709202fce5ec
cybuildguild <macn51@proton.me>
2026-02-01 18:12:49 -0500

    Create Structure of LoginPage.js File

---
+++ src/Components/LoginPage/LoginPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class LoginPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { }
+    }
+    render() {
+        return(
+            <div> Hello World</div>
+        )
+    }
+}
+export default LoginPage;
