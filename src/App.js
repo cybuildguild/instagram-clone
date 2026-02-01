6f5675e352316875c494ba708cf764d1f140dfe1
cybuildguild <macn51@proton.me>
2026-02-01 18:13:13 -0500

    Import LoginPage in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
+import LoginPage from './Components/LoginPage/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      
+      <LoginPage/>
     </div>
   );
 }
