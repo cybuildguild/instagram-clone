7244ba9aa7e64a5412d3d76c7b5af35ab4cd0b6f
cybuildguild <macn51@proton.me>
2026-02-01 22:39:47 -0500

    Create Structure for HomePage.js

---
+++ src/Components/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import './HomePage.css';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state =  { }
+    }
+    render() {
+        return (
+            <div>Hello from CyBuildGuild</div>
+        );
+    }
+}
+export default Home;
