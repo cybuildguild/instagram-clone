d3dfb49b076b38f9475aece7b1c786429b71fcc1
cybuildguild <macn51@proton.me>
2026-02-01 22:45:23 -0500

    Create Structure for NavBar.JS

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import './NavBar.css';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state =  { }
+    }
+    render() {
+        return (
+            <div>NavBar</div>
+        );
+    }
+}
+export default NavBar;
