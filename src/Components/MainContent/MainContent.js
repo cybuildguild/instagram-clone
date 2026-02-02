0c430cbf369f71740a4b8a3f84a293f387889592
cybuildguild <macn51@proton.me>
2026-02-01 23:22:43 -0500

    Create Grid structure in MainContent.js

--- src/Components/MainContent/MainContent.js
+++ src/Components/MainContent/MainContent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import './MainContent.css';
+import Grid from "@mui/material/Grid";
 
 class MainContent extends Component {
     constructor(props) {
@@ -9,6 +10,21 @@ class MainContent extends Component {
     render() {
         return (
             <div>
+                <Grid container>
+                    <Grid item={2}></Grid>
+                    <Grid item={6}>
+                        <div>
+                            
+                        </div>
+                    </Grid>
+
+                    <Grid>
+                        <Grid item={2}></Grid>
+                    </Grid>
+                    <Grid>
+                        <Grid item={2}></Grid>
+                    </Grid>
+                </Grid>
             </div>
             
         );
