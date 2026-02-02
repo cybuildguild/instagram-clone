b8e7a91a7ce5e756de7494cf809ec93d065aa03c
cybuildguild <macn51@proton.me>
2026-02-01 22:47:53 -0500

    Create Structure for MainContent JS File

---
+++ src/Components/MainContent/MainContent.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import './MainContent.css';
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state =  { }
+    }
+    render() {
+        return (
+            <div>
+                Main Content
+            </div>
+            
+        );
+    }
+}
+export default MainContent;
