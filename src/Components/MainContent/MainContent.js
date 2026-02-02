62725740e98321a9e6d5b70d85dd173404005fda
cybuildguild <macn51@proton.me>
2026-02-01 23:29:05 -0500

    Create Structure for StatusBar.js

--- src/Components/MainContent/MainContent.js
+++ src/Components/MainContent/MainContent.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import './MainContent.css';
 import Grid from "@mui/material/Grid";
-import StatusBar from '../StatusBar/StatusBar.css';
-import Mainpage from '../MainPage/MainPage.css';
+import StatusBar from '../StatusBar/StatusBar';
+import Mainpage from '../MainPage/MainPage';
 
 class MainContent extends Component {
     constructor(props) {
---
+++ src/Components/StatusBar/StatusBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import './StatusBar.css'
+
+class StatusBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state =  { }
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+
+}
+export default StatusBar;
